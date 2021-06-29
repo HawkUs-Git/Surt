800f5ca5cae80a2065f20050211aecb40e8ac5e7
Hawkus <77986031+HawkUs-Git@users.noreply.github.com>
2021-06-29 14:50:39 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,35 @@
+if (annyang) {
+  // Let's define a command.
+  const commands = {
+    'hello': () => { alert('Hello world!'); },
+    'go to blog': () => { window.location.href = 'https://blog.marcusj.tech' },
+    'Go to Query': () => { window.location.href = 'https://query.arjun418.repl.co/' },
+    'ask (me) my *thing': (thing) => { prompt('What is your ' + thing + '?') },
+    'Who Made this': () => { alert('Arjun418 did!'); },
+    'tell me (for) *query': (query) => { window.location.href = 'https://google.com/search?q=' + query },
+    'search (for) *query': (query) => { window.location.href = 'https://google.com/search?q=' + query },
+    'see (for) *query': (query) => { window.location.href = 'https://google.com/search?q=' + query },
+    'sound': () => {var audio = new Audio('audio_file.mp3');
+audio.play(); },
+    'calculate :quarter stats': {'regexp': /^calculate (January|April|July|October) stats$/, 'callback': console.log},
+  };
+
+  // Add our commands to annyang
+  annyang.addCommands(commands);
+
+  annyang.addCallback('start', function() {
+      document.getElementById('running').innerText = 'True';
+  })
+  annyang.addCallback('end', function() {
+      document.getElementById('running').innerText = 'False';
+  })
+
+  annyang.addCallback('resultMatch', function(phrase, match, alt) {
+      document.getElementById('phrase').innerText = phrase;
+      document.getElementById('match').innerText = match;
+      document.getElementById('alt').innerText = alt;
+  })
+
+  // Start listening.
+  annyang.start();
+}
