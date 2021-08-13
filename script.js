29b6cb35e74bca231dc34b71db5f9b3aa292285f
Hawkus <77986031+HawkUs-Git@users.noreply.github.com>
2021-08-13 21:00:00 +0000

    All New Stuff

--- script.js
+++ script.js
@@ -4,12 +4,15 @@ if (annyang) {
     'hello': () => { alert('Hello world!'); },
     'go to blog': () => { window.location.href = 'https://blog.marcusj.tech' },
     'Go to Query': () => { window.location.href = 'https://query.arjun418.repl.co/' },
-    'ask (me) my *thing': (thing) => { prompt('What is your ' + thing + '?') },
+    'amma ask (me) my *thing': (thing) => { prompt('What is your ' + thing + '?') },
     'Who Made this': () => { alert('Arjun418 did!'); },
     'tell me (for) *query': (query) => { window.location.href = 'https://google.com/search?q=' + query },
     'search (for) *query': (query) => { window.location.href = 'https://google.com/search?q=' + query },
-    'see (for) *query': (query) => { window.location.href = 'https://google.com/search?q=' + query },
-    'sound': () => {var audio = new Audio('audio_file.mp3');
+    '*query on amazon': (query) => { window.location.href = 'https://www.amazon.com/s?k=' + query },
+
+
+    'amma see (for) *query': (query) => { window.location.href = 'https://google.com/search?q=' + query },
+    'amma sound': () => {var audio = new Audio('audio_file.mp3');
 audio.play(); },
     'calculate :quarter stats': {'regexp': /^calculate (January|April|July|October) stats$/, 'callback': console.log},
   };
